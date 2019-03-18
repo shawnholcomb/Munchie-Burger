5c169184fc3ec08662d844211c7a821c1adbcace
Shawn Holcomb <sholcmb@gmail.com>
2019-03-17 19:12:10 -0500

    update connection for clearDB

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Ft041104",
-  database: "burgers_db"
+  host: "us-cdbr-iron-east-03.cleardb.net",
+  user: "b86d6c5651e4e7",
+  password: "83ed091a",
+  database: "heroku_d2a6fa44d49ff75"
 });
 
 connection.connect(function(err) {
