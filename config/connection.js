417caeef674b42fc09ab73546faf964b5126d800
Shawn Holcomb <sholcmb@gmail.com>
2019-03-19 18:21:08 -0500

    fix connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var connection;
 
 // Server for ClearDB database
 if (process.env.JAWSDB_URL) {
-  connection.mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
